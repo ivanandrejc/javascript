f0af5098ac6daa6684140247d68f4d24be957cdb
ivan <93165547+ivanandrejc@users.noreply.github.com>
2022-09-14 20:25:32 -0300

    Add files via upload

--- app.js
+++ app.js
@@ -1,12 +1,11 @@
-let ropa = prompt (`Que prende le gustaria adquirir?
+/*let ropa = prompt (`Que prende le gustaria adquirir?
 Las opciones son: 
 buzo
 pantalon
 remera
 buzo y remera
 pantalon y remera
-combo completo (25% off)`);
-
+combo completo (25% off)`);*/
 
 class producto {
     constructor(nombre , precio){
@@ -24,7 +23,9 @@ const producto1 = new producto ("buzo", "7000");
 const producto2 = new producto ("pantalon", "5500");
 const producto3 = new producto ("remera", "3500");
 
-switch (ropa){
+let productos = [producto]
+
+/*switch (ropa){
     case "buzo":
         producto1.sumaiva(); 
         console.log(producto1);
@@ -64,4 +65,86 @@ switch (ropa){
         total = parseFloat(total - (descuento/100*total))
         console.log("su total es : ", total);
         break;                      
+}*/
+let agregar = 0 ;
+function capturar(){
+    
+    
+    var nombreproducto = document.getElementById("Nombre").value;
+    
+    var carrito = [];
+    
+
+    switch (nombreproducto){
+        case "buzo":
+            producto1.sumaiva(); 
+            console.log(producto1);
+            console.log("su total es : ", producto1.precio);
+            for(a of productos){
+                let contenedor = document.createElement("div");
+                contenedor.innerHTML = `<h3>Producto: ${producto1.nombre}</h3>
+                <p>Precio: ${producto1.precio}</p>
+                `
+                document.body.appendChild(contenedor);
+            }
+            agregar = producto1.precio
+            break;
+        case "pantalon":
+            producto2.sumaiva(); 
+            console.log(producto2);
+            console.log("su total es : ", producto2.precio);
+            for(a of productos){
+                let contenedor = document.createElement("div");
+                contenedor.innerHTML = `<h3>Producto: ${producto2.nombre}</h3>
+                <p>Precio: ${producto2.precio}</p>
+                `
+                document.body.appendChild(contenedor);
+            }
+            break;
+        case "remera":
+            producto3.sumaiva(); 
+            console.log(producto3);
+            console.log("su total es : ", producto3.precio);
+            for(a of productos){
+                let contenedor = document.createElement("div");
+                contenedor.innerHTML = `<h3>Producto: ${producto3.nombre}</h3>
+                <p>Precio: ${producto3.precio}</p>
+                `
+                document.body.appendChild(contenedor);
+            }
+            break;
+        case "buzo y remera":
+            producto1.sumaiva();
+            producto3.sumaiva(); 
+            console.log(producto1,producto3);
+            total = parseFloat(producto1.precio + producto3.precio);
+            console.log("su total es : ", total);
+            break;
+        case "pantalon y remera":
+            producto2.sumaiva();
+            producto3.sumaiva(); 
+            console.log(producto2,producto3);
+            total = parseFloat(producto2.precio + producto3.precio);
+            console.log("su total es : ", total);
+            break;
+        case "combo completo":
+            producto1.sumaiva();
+            producto2.sumaiva();
+            producto3.sumaiva(); 
+            console.log(producto1,producto2,producto3);
+            descuento = 25
+            total = parseFloat(producto1.precio + producto2.precio + producto3.precio);
+            total = parseFloat(total - (descuento/100*total))
+            console.log("su total es : ", total);
+            break;                      
+    }
+    
 }
+
+
+
+
+/*function agregar(){
+    carrito.push({nombre,precio});
+    console.log(carrito);
+}*/
